d63e9df24b3de485cfd15f5c7bc687a49a52f3a2
MatrixMagician <ohingst@gmail.com>
2025-06-16 14:24:36 +0100

    Added file player.py player logic and modified constants.py to add player radius and modified main.py to draw the player

--- main.py
+++ main.py
@@ -1,35 +1,31 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
     # Initialise pygame
     pygame.init()
-
     # Create the game window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    # Create player instance after filling the screen but before flipping the display
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     # Basic game loop
     while True:
         # Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         # Fill the screen with black
         screen.fill("black")
-        
+        # Draw the player
+        player.draw(screen)
         # Refresh the screen
         pygame.display.flip()
-
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
