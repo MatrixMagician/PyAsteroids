cd23f41394b87df8b6202694726851cb5cc93b2f
MatrixMagician <ohingst@gmail.com>
2025-06-18 15:02:19 +0100

    Updated main.py to use Groups.

--- main.py
+++ main.py
@@ -6,14 +6,24 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     # Initialise pygame
     pygame.init()
+
     # Create the game window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    # Create sprite groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
     # Create player instance after filling the screen but before flipping the display
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable.add(player)
+    drawable.add(player)
+
     # Basic game loop
     while True:
         # Handle events
@@ -23,13 +33,16 @@ def main():
         # Fill the screen with black
         screen.fill("black")
 
-        # Update the player (handle input and rotation)
-        player.update(dt)
+        # Update all updatable objects
+        updatable.update(dt)
 
-        # Draw the player
-        player.draw(screen)
+        # Draw all drawable objects
+        for sprite in drawable:
+            sprite.draw(screen)
+
         # Refresh the screen
         pygame.display.flip()
+
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
